d3977f0a645d33870a03234ca29e5c6f0de693b1
Lucky Sah <lk4179869@gmail.com>
2026-09-23 16:42:04 +0530

    add: put method

--- 01-basics/app.js
+++ 01-basics/app.js
@@ -25,11 +25,37 @@ app.post("/users", async (req,res)=>{
 
 app.get("/getUsers", async(req,res)=>{
     try{
-        let data= await User.findOne({name:"lucky Kumar"});
+        //basic operations
+        // let data= await User.findOne({name:"lucky Kumar"});
+        // let data= await User.find({age:{$gte:20}});
+        // let data= await User.find({age:{$in:[18,20,30]}})
+        // let data= await User.find({$or : [{name:"lucky Kumar"},{name:"rahul"}]})
+        // let data= await User.find({$and: [{name: "lucky Kumar"},{age:20}]})
+        // let data= await User.find({$and: [{age:{$gt:19}}, {age:{$gte:20}}]})
+        // let data= await User.find({}, "name followers")
+        // let data= await User.find().select("name age")
+        // let data= await User.find().sort({followers:-1})
+        // let data= await User.find().sort({followers:-1}).limit(1)
+
+        //UPDATE RELATED:-
+        const id= await User.find().select("id").limit(1);
+        console.log(id)
+        const data= await User.findByIdAndUpdate(id, {name:"rocky Kumar"})
+
         res.json(data)
         console.log(data)
     }catch(err){
         console.log(err);
+    }
+})
+
+app.put("/user/:id", async(req,res)=>{
+    try{
+        const data= await User.findByIdAndUpdate(req.params.id,req.body);
+        res.json(data);
+    }
+    catch(err){
+        res.status(500).json({error: err.message})
     }
 })
 
